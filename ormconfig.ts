dbebe5b008e2200ee767ee0b5cb3ec40ab551946
Anderson Rocha <andersoncristiano@frwk.com.br>
2021-10-16 14:36:25 -0300

    Fix build

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,6 +1,6 @@
 module.exports = {
   type: "postgres",
-  url: process.env.DATABASE_URL,
+  url: `${process.env.DATABASE_URL}?sslmode=require`,
   entities: ['src/app/models/*.ts'],
   migrations: ['src/database/migrations/*.ts'],
   cli: {
