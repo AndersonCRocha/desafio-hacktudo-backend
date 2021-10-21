7cd700cee10a58e363e596ae7123602e6ed4e36d
Anderson Rocha <andersoncristiano@frwk.com.br>
2021-10-21 01:18:08 -0300

    fix: change ormconfig to use builded files

--- dist/ormconfig.js
+++ dist/ormconfig.js
@@ -1,11 +1,22 @@
 "use strict";
+const entities = [
+    process.env.PRODUCTION ? "dist/src/app/models/*.js" : "src/app/models/*.ts",
+];
+const migrations = [
+    process.env.PRODUCTION
+        ? "dist/src/database/migrations/*.js"
+        : "src/database/migrations/*.ts",
+];
+const migrationsDir = process.env.PRODUCTION
+    ? "dist/src/database/migrations"
+    : "src/database/migrations";
 module.exports = {
     type: "postgres",
     url: process.env.DATABASE_URL,
-    entities: ["src/app/models/*.ts"],
-    migrations: ["src/database/migrations/*.ts"],
+    entities,
+    migrations,
     cli: {
-        migrationsDir: "src/database/migrations",
+        migrationsDir,
     },
     migrationsRun: true,
     uuidExtension: "uuid-ossp",
