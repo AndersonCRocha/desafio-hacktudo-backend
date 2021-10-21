aac5d92241b76d76e31f6f47e3b0dd42eb22274f
Anderson Rocha <andersoncristiano@frwk.com.br>
2021-10-21 00:57:50 -0300

    fix: change ormconfig

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,15 +1,11 @@
-import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
-
-const config: PostgresConnectionOptions = {
+module.exports = {
   type: "postgres",
   url: process.env.DATABASE_URL,
-  entities: ['src/app/models/*.ts'],
-  migrations: ['src/database/migrations/*.ts'],
+  entities: ["src/app/models/*.ts"],
+  migrations: ["src/database/migrations/*.ts"],
   cli: {
-    migrationsDir: 'src/database/migrations',
+    migrationsDir: "src/database/migrations",
   },
   migrationsRun: true,
-  uuidExtension: "uuid-ossp"
-}
-
-module.exports = config
+  uuidExtension: "uuid-ossp",
+};
