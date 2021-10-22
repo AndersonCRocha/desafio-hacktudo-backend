45d332786014e29f8fc266bf60dbada6178a7671
Anderson Rocha <andersoncristiano@frwk.com.br>
2021-10-22 14:15:49 -0300

    feat: enable cors
    fix: fixing cors options
    
    fix: fixing cors
    
    fix: fixing cors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 
-import './database/connect';
+import "./database/connect";
 
-import { router } from './app/routes';
+import { cors } from "./app/middlewares/cors";
 
-dotenv.config()
+import { router } from "./app/routes";
+
+dotenv.config();
 
 express()
   .use(express.json())
+  .use(cors)
   .use(router)
-  .listen(process.env.PORT, () => console.log(`🔥 Server running in http://localhost:${process.env.PORT}`));
+  .listen(process.env.PORT, () =>
+    console.log(`🔥 Server running in http://localhost:${process.env.PORT}`)
+  );
