f6cc8f0ef334cf616cedee1c37b7c32391c551c3
Anderson Rocha <andersoncristiano@frwk.com.br>
2021-10-22 14:50:35 -0300

    fix: fixing cors

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors, { CorsOptions } from "cors";
+import cors from "cors";
 
-import "./database/connect";
+// import "./database/connect";
 
 import { router } from "./app/routes";
 
 dotenv.config();
 
-const corsOptions: CorsOptions = {
-  origin: "*",
-  methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
-};
-
 express()
-  .use(cors(corsOptions))
+  .use(cors())
   .use(express.json())
   .use(router)
   .listen(process.env.PORT, () =>
