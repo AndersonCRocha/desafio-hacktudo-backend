e0a583213e07e08630124b936e5d52d8a47a5d63
Anderson Rocha <andersoncristiano@frwk.com.br>
2021-10-22 14:15:49 -0300

    feat: enable cors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 
-import './database/connect';
+import "./database/connect";
 
-import { router } from './app/routes';
+import { router } from "./app/routes";
 
-dotenv.config()
+dotenv.config();
 
 express()
+  .use(cors())
   .use(express.json())
   .use(router)
-  .listen(process.env.PORT, () => console.log(`🔥 Server running in http://localhost:${process.env.PORT}`));
+  .listen(process.env.PORT, () =>
+    console.log(`🔥 Server running in http://localhost:${process.env.PORT}`)
+  );
